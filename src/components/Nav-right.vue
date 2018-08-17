1153b5d819b2acdcf542260003f31c9713982f17
decoster <timothee.decoster@elosi.com>
2018-08-17 10:42:40 +0200

    Sections svg + css

--- src/components/Nav-right.vue
+++ src/components/Nav-right.vue
@@ -2,7 +2,11 @@
     <div class="nav-right">
         <img src="@/assets/univ_logo.png" alt="Logo de l'Université" width="100%">
         <ul>
-            <router-link v-for="lvl in section" :key="lvl" :to="`${lvl.path}`" tag="li">
+            <router-link v-for="(lvl, index) in section" :key="index" :to="`${lvl.path}`" tag="li" 
+            @click.native="selectSection(lvl.name)" v-bind:class="{ selected: select === lvl.name }">
+                <svg viewBox="0 0 20 20" class="icon">
+                    <path d="M0 0 L10 10 L0 20"></path>
+                </svg>
                 {{ lvl.name }}
             </router-link>
         </ul>
@@ -14,6 +18,7 @@ export default {
     name: 'NavRight',
     data() {
         return {
+            select: 'Accueil',
             section: [
                 { path: '/', name: 'Accueil' },
                 { path: '/L1', name: 'Licence 1' },
@@ -23,6 +28,11 @@ export default {
                 { path: '/M2', name: 'Master 2' }
             ]
         };
+    },
+    methods: {
+        selectSection(name) {
+            this.select = name;
+        }
     }
 };
 </script>
@@ -48,9 +58,29 @@ ul {
     padding: 0;
 }
 li {
+    text-align: left;
     background-color: rgb(255, 255, 255);
     font-size: 3vh;
     cursor: pointer;
     margin: 0.1vh 0 0 0;
+}
+.selected .icon {
+    opacity: 1;
+    transform: translateX(0);
+}
+.icon {
+    width: 1rem;
+    opacity: 0;
+    transition: all 0.2s;
+    transform: translateX(-100%);
+}
+.icon path {
+    stroke: #cdcdcd;
+    stroke-width: 5px;
+    fill: none;
+}
+li:hover .icon {
+    opacity: 1;
+    transform: translateX(0);
 }
 </style>
